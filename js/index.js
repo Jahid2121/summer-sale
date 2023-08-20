2805ad3f94898c4cd44a0a94a8fee60345ffb221
Jahid2121 <jahidsarkar2121@gmail.com>
2023-08-20 08:21:57 +0600

    added product in selected-product when clicked

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
-document.getElementById('product-1').addEventListener('click', function(){
-    const title = target.
-})
+function handleProductCard(target){
+    const selectedItemContainer = document.getElementById('selected-items')
+    const productName = target.childNodes[3].childNodes[5].innerText
+    const li = document.createElement('li')
+    li.innerText = productName;
+    selectedItemContainer.appendChild(li)
+}
