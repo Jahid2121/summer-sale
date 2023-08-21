64d8772953810e5bf1c0926fec970ad2ffacd25d
Jahid2121 <jahidsarkar2121@gmail.com>
2023-08-21 08:22:42 +0600

    updated go home button

--- js/goHome.js
+++ js/goHome.js
@@ -1,6 +1,16 @@
-document.getElementById('go-home').addEventListener('click', function(){
-    const totalPrice = getInnerText('total')
-    console.log(totalPrice);
+const field = document.getElementById('go-home')
+field.addEventListener('click', function(){
+    const totalField = document.getElementById('total')
+totalField.innerText = ''
+
+const selectedProductContainer = document.getElementById('selected-product')
+selectedProductContainer.innerText = ''
+const inputField = document.getElementById('coupon-field')
+    inputField.value = ''
+    const allTotalField = document.getElementById('allTotal')
+    allTotalField.innerText = ''
+    const discField = document.getElementById('discount-field')
+    discField.innerText = ''
 })
 
 
